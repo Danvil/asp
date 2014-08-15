27902c534e948f2470661b8ebd15f0a7f0d4a3d2
danvil <davidw@danvil.de>
2014-08-15 12:45:29 +0200

    implemented asp properly

--- src/asp/main.cpp
+++ src/asp/main.cpp
@@ -10,15 +10,18 @@
 
 int main(int argc, char** argv)
 {
-	std::string p_img_rgb = "";
-	std::string p_img_d = "";
+	std::string p_fn_color;
+	std::string p_fn_depth;
+	std::string p_method;
+
 
 	namespace po = boost::program_options;
 	po::options_description desc;
 	desc.add_options()
 		("help", "produce help message")
-		("rgb", po::value(&p_img_rgb), "path to input color image")
-		("depth", po::value(&p_img_d), "path to input depth image")
+		("color", po::value(&p_fn_color), "path to input color image")
+		("depth", po::value(&p_fn_depth), "path to input depth image")
+		("method", po::value(&p_method)->default_value("SLIC"), "superpixel method: SLIC, DASP")
 	;
 
 	po::variables_map vm;
@@ -29,21 +32,38 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	slimage::Image3ub img_input = slimage::Load3ub(p_img_rgb);
-	slimage::GuiShow("input", img_input);
-
-	// {
-	// 	auto sp = asp::SLIC(img_input);
-	// 	slimage::gui::Show("slic", PlotColor(sp));
-	// }
-
-	{
-		slimage::Image1ui16 img_depth = slimage::Load1ui16(p_img_d);
-		auto sp = asp::DASP(img_input, img_depth);
-		slimage::GuiShow("dasp", PlotColor(sp));
+	if(p_method == "SLIC") {
+		// load data
+		slimage::Image3ub img_color = slimage::Load3ub(p_fn_color);
+		slimage::GuiShow("color", img_color);
+		// compute superpixels
+		auto sp = asp::SLIC(img_color);
+		// visualize superpixels
+		slimage::GuiShow("slic", PlotColor(sp));
+		slimage::GuiWait();
 	}
-
-	slimage::GuiWait();
+	else if(p_method == "DASP") {
+		// load data
+		slimage::Image3ub img_color = slimage::Load3ub(p_fn_color);
+		slimage::GuiShow("color", img_color);
+		slimage::Image1ui16 img_depth = slimage::Load1ui16(p_fn_depth);
+		{
+			slimage::Image1ui16 img_depth_vis = img_depth;
+			for(auto& p : img_depth_vis) {
+				p *= 16;
+			}
+			slimage::GuiShow("depth", img_depth_vis);
+		}
+		// compute superpixels
+		auto sp = asp::DASP(img_color, img_depth);
+		// visualize superpixels
+		slimage::GuiShow("dasp", PlotColor(sp));
+		slimage::GuiWait();
+	}
+	else {
+		std::cerr << "Unknown method. Use --h for help." << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
